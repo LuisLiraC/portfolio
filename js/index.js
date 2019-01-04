508bebd0eb8aa9ce4e8d00fbc06ef129099363de
LuisLiraC <luis_ssbm@hotmail.com>
2019-01-04 02:06:50 -0600

    Cargar cursos desde JS

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const $hideModal = document.getElementById('hideModal');
 const $modalImg = document.getElementById('modal-img');
 
 function openCourse(event){
-    var imgClick = event.currentTarget.getAttribute('src');
+    var imgClick = event.currentTarget.dataset.img;
     $modal.classList.add('active');
     $modalImg.setAttribute('src', imgClick);
     $modalContent.style.animation = 'modalIn .8s forwards';
@@ -25,7 +25,50 @@ $hideModal.addEventListener('click', () => {
     }, 800);
 });
 
-function fadeIn(event){
-    event.currentTarget.style = 'display: block;'
-    event.currentTarget.style.animation = 'fadeIn 1.5s forwards';
-}
+function courseTemplate (HTMLString){
+    const html = document.implementation.createHTMLDocument();
+    html.body.innerHTML = HTMLString;
+    return html.body.children[0];
+};
+
+const $test = document.getElementById('courses-container');
+const folder = 'img/courses/';
+
+const course_images =[
+    // Frontend
+    'diploma-jquery-js.png',
+    'diploma-optimizacion-web.png',
+    'diploma-postcss.png',
+    'diploma-animaciones-web.png',
+    'diploma-responsive-design.png',
+    'diploma-css-grid-layout.png',
+    'diploma-html5-css3.png',
+    'diploma-programacion-basica.png',
+    // Conocimientos Generales
+    'diploma-git-github.png',
+    'diploma-terminal.png',
+    'diploma-ingenieria.png',
+    'diploma-redes.png',
+    'diploma-marca-personal.png',
+    // Inglés
+    'diploma-ingles-basico.png',
+    'diploma-ingles-gramatica.png',
+    'diploma-ingles-conversacion.png'
+]
+
+course_images.forEach(function(img) {
+    const course_template = `
+    <figure class="course" onclick="openCourse(event)" data-img="${folder}${img}">
+        <img src="${folder}${img}">
+    </figure>
+    `
+    const course_element = courseTemplate(course_template);
+    const image = course_element.querySelector('img')
+    
+    image.addEventListener('load', (event) => {
+        event.srcElement.style = 'display: inline-block';
+        event.srcElement.style.animation = 'fadeIn 1.5s forwards'
+    })
+    $test.appendChild(course_element);
+})
+
