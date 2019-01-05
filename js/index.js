6f212c8816fd9a4541f33e05b590fd6e93a91c80
LuisLiraC <luis_ssbm@hotmail.com>
2019-01-05 07:56:45 -0600

    No dormi :)

--- js/index.js
+++ js/index.js
@@ -4,11 +4,59 @@ const $modalContent = document.getElementById('modal-content');
 const $hideModal = document.getElementById('hideModal');
 const $modalImg = document.getElementById('modal-img');
 
+
+const $summary = document.getElementById('summary');
+const $jobModal = document.getElementById('job-modal');
+const $jobModalContent = document.getElementById('job-modal-content');
+const $jobModalImg = document.getElementById('job-modal-img');
+const $jobModalTitle = document.getElementById('job-modal-title');
+const $jobModalPosition = document.getElementById('job-modal-position');
+const $activities = document.getElementById('activities');
+const $jobHideModal = document.getElementById('jobHideModal');
+
 function openCourse(event){
     var imgClick = event.currentTarget.dataset.img;
     $modal.classList.add('active');
     $modalImg.setAttribute('src', imgClick);
     $modalContent.style.animation = 'modalIn .8s forwards';
+}
+
+function openSummary (event) {
+    var jobClick = event.currentTarget.dataset.job;
+    
+    const jobData = findJob(jobClick)
+    
+    $jobModal.classList.add('active');
+    $jobModalImg.setAttribute('srcset', jobData.children[0].children[0].children[0].getAttribute('srcset'))
+    $jobModalTitle.innerHTML = jobData.children[0].children[1].innerHTML
+    $jobModalPosition.innerHTML = jobData.children[0].children[2].innerHTML
+    $activities.innerHTML = getActivities(jobClick)
+    $jobModalContent.style.animation = 'modalIn .8s forwards';
+}
+
+function findJob(job) {
+    const finded = document.getElementById(job)
+    return finded
+}
+
+function getActivities(job){
+    switch (job){
+        case "job1": 
+            return `Postulación de vacantes, Búsqueda de candidatos, Entrevistas vía telefónica, skype o presenciales, screening de candidatos, Seguimiento a proceso de contratación.`
+        case "job2":
+            return `Desarrollo de aplicación de escritorio con Visual Basic .Net
+                    Aplicación para la gestión de pagos de aplicadores de Estudios Socioeconómicos</br>
+                    </br>
+                    Uso de Report Viewer para crear reportes de pagos por persona, por panco y general.</br>
+                    Almacenamiento en Base de Datos Microsoft Access 2016.`
+        case "job3":
+            return `<strong>Reclutamiento:</strong></br>
+                    Postulación de vacantes, Búsqueda de candidatos, Entrevistas, Encargado de plataforma de psicometrías en línea, Trato directo con clientes, Reportes mensuales de resultados de departamente de Estudios Socioeconómicos, Seguimiento a proceso de contratación.</br>
+                    </br>
+                    <strong>Soporte Técnico:</strong></br>
+                    Atención a problemas de usuarios internos, Formateo de equipos, Instalación de Sistema Operativo, Software y Periféricos, Implementación de respaldos automáticos, Creación de tutoriales para aplicación móvil, Creación de formatos en Excel (Fórmulas, Tablas dinámicas, Formatos condicionales, etc).
+                    </br></br>`
+    }
 }
 
 $modal.addEventListener('click', () => {
@@ -18,10 +66,24 @@ $modal.addEventListener('click', () => {
     }, 800);
 });
 
+$jobModal.addEventListener('click', () => {
+    $jobModalContent.style.animation = 'modalOut .8s forwards';
+    setTimeout(function() {
+        $jobModal.classList.remove('active')
+    }, 800);
+});
+
 $hideModal.addEventListener('click', () => {
     $modalContent.style.animation = 'modalOut .8s forwards';
     setTimeout(function() {
         $modal.classList.remove('active')
+    }, 800);
+});
+
+$jobHideModal.addEventListener('click', () => {
+    $jobModalContent.style.animation = 'modalOut .8s forwards';
+    setTimeout(function() {
+        $jobModal.classList.remove('active')
     }, 800);
 });
 
@@ -62,6 +124,7 @@ const course_images =[
     'diploma-ingles-conversacion.png'
 ]
 
+$courses_title.innerHTML = `Cursos aprobados: ${course_images.length}`
 course_images.forEach(function(img) {
     const course_template = `
     <figure class="course" onclick="openCourse(event)" data-img="${folder}${img}">
@@ -76,7 +139,6 @@ course_images.forEach(function(img) {
         event.srcElement.style.animation = 'fadeIn 1.5s forwards'
     })
     $courses_container.append(course_element);
-    $courses_title.innerHTML = `Cursos aprobados: ${course_images.length}`
 })
 
 
